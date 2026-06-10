830638c736e509ab59ac74002b297b7c79d2d43f
Zachary Heil <Zach@zhsystems.ca>
2026-06-10 10:01:38 -0400

    Remove IntelliJ dependants

---
+++ src/main/java/org/jetbrains/annotations/NotNull.java
@@ -0,0 +1,11 @@
+package org.jetbrains.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD, ElementType.LOCAL_VARIABLE, ElementType.TYPE_USE, ElementType.TYPE_PARAMETER})
+public @interface NotNull {
+}
---
+++ src/main/java/org/jetbrains/annotations/VisibleForTesting.java
@@ -0,0 +1,11 @@
+package org.jetbrains.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD, ElementType.FIELD, ElementType.CONSTRUCTOR, ElementType.TYPE})
+public @interface VisibleForTesting {
+}
